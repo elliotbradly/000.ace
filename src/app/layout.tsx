4c428ff6b43b913eace47dbbfb38d9c13a99cdc2
Brad Henderson <elliotbradly@gmail.com>
2025-07-06 12:45:29 -0400

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,8 @@
 import type { Metadata } from "next";
 
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-
-import Redirect from "./Redirect";
-
 import Providers from "@/lib/query-provider";
-
-import localFont from "next/font/local";
-import Head from "next/head";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "../../fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "../../fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Next App Mantine Tailwind Template",
@@ -41,26 +16,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <Providers>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          </header>
+          {children}
+        </Providers>
+      </body>
+    </html>
 
-        <body className="antialiased">
-
-          <Providers>
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton  />
-              </SignedOut>
-              <SignedIn>
-                <Redirect/>
-              </SignedIn>
-            </header>
-            {children}
-          </Providers>
-
-        </body>
-      </html>
-    </ClerkProvider>
   );
 }
